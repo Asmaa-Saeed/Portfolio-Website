2c88fae9caa5b0cb6b0f83b1f62edb6b1e3cf331
Asmaa-Saeed <asmaasaeed2082003@gmail.com>
2024-08-26 14:07:22 +0300

    upload header section

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ function openMenu() {
 function closeMenu() {
     sideMenu.style.transform = 'translateX(16rem)';
 }
+
+
+// auto typing function
+ 
+document.addEventListener("DOMContentLoaded", function() {
+    const textElement = document.getElementById('typing-text');
+    const texts = ["Flutter Developer Based In Egypt", "Hard Working developer"];
+    let textIndex = 0;
+    let charIndex = 0;
+
+    function type() {
+        if (charIndex < texts[textIndex].length) {
+            textElement.textContent += texts[textIndex].charAt(charIndex);
+            charIndex++;
+            setTimeout(type, 100); // Adjust typing speed by changing the delay
+        } else {
+            setTimeout(() => {
+                textElement.textContent = ''; // Clear text after finishing
+                charIndex = 0;
+                textIndex = (textIndex + 1) % texts.length; // Move to the next text, loop back to start
+                type(); // Restart typing
+            }, 2000); // Wait before starting the next text
+        }
+    }
+
+    type(); // Start the typing effect
+});

ec2e44d63678c1a2b70a8a1dcdc9ebb9f0b01494
Asmaa-Saeed <asmaasaeed2082003@gmail.com>
2024-08-28 09:28:41 +0300

    add smooth fill using css and js to skills section

--- script.js
+++ script.js
@@ -1,35 +1,45 @@
-const sideMenu = document.querySelector('#sideMenu');
+const sideMenu = document.querySelector("#sideMenu");
 
 function openMenu() {
-    sideMenu.style.transform = 'translateX(-16rem)';
+  sideMenu.style.transform = "translateX(-16rem)";
 }
 function closeMenu() {
-    sideMenu.style.transform = 'translateX(16rem)';
+  sideMenu.style.transform = "translateX(16rem)";
 }
 
-
 // auto typing function
- 
-document.addEventListener("DOMContentLoaded", function() {
-    const textElement = document.getElementById('typing-text');
-    const texts = ["Flutter Developer Based In Egypt", "Hard Working developer"];
-    let textIndex = 0;
-    let charIndex = 0;
 
-    function type() {
-        if (charIndex < texts[textIndex].length) {
-            textElement.textContent += texts[textIndex].charAt(charIndex);
-            charIndex++;
-            setTimeout(type, 100); // Adjust typing speed by changing the delay
-        } else {
-            setTimeout(() => {
-                textElement.textContent = ''; // Clear text after finishing
-                charIndex = 0;
-                textIndex = (textIndex + 1) % texts.length; // Move to the next text, loop back to start
-                type(); // Restart typing
-            }, 2000); // Wait before starting the next text
-        }
+document.addEventListener("DOMContentLoaded", function () {
+  const textElement = document.getElementById("typing-text");
+  const texts = ["Flutter Developer Based In Egypt", "Hard Working developer"];
+  let textIndex = 0;
+  let charIndex = 0;
+
+  function type() {
+    if (charIndex < texts[textIndex].length) {
+      textElement.textContent += texts[textIndex].charAt(charIndex);
+      charIndex++;
+      setTimeout(type, 100); // Adjust typing speed by changing the delay
+    } else {
+      setTimeout(() => {
+        textElement.textContent = ""; // Clear text after finishing
+        charIndex = 0;
+        textIndex = (textIndex + 1) % texts.length; // Move to the next text, loop back to start
+        type(); // Restart typing
+      }, 2000); // Wait before starting the next text
     }
+  }
 
-    type(); // Start the typing effect
+  type(); // Start the typing effect
+});
+
+
+
+// add the skills smooth fill
+document.addEventListener("DOMContentLoaded", function () {
+  const progressBars = document.querySelectorAll(".the-progress span");
+  progressBars.forEach(function (Bar) {
+    const width = Bar.getAttribute("data-width");
+    Bar.style.width = width;
+  });
 });
